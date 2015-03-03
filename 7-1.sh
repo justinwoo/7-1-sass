2ad56fa98924a626e5310e2ddaca24d09c48d6af
Justin Woo <moomoowoo@gmail.com>
2015-03-03 12:07:21 -0500

    i need some gitkeepSSSSSSSSSSSSSSSSSSSSS

--- 7-1.sh
+++ 7-1.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-mkdir base
-mkdir components
-mkdir layout
-mkdir pages
-mkdir themes
-mkdir utils
-mkdir vendors
+mkdir -p base
+touch base/.gitkeep
+mkdir -p components
+touch components/.gitkeep
+mkdir -p layout
+touch layout/.gitkeep
+mkdir -p pages
+touch pages/.gitkeep
+mkdir -p themes
+touch themes/.gitkeep
+mkdir -p utils
+touch utils/.gitkeep
+mkdir -p vendors
+touch vendors/.gitkeep
 touch main.scss
